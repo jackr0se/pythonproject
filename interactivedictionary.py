22c596042be69f62a125fd902f7e03ac24a83a9d
jackr0se <69627202+jackr0se@users.noreply.github.com>
2022-05-11 20:08:16 +0700

    Update interactivedictionary.py

--- interactivedictionary.py
+++ interactivedictionary.py
@@ -8,20 +8,26 @@ def translate(w):
 	if w in data:
 		return data[w]
 	elif len(get_close_matches(w,data.keys())) > 0:
-		yesno = input ("Did you mean %s instead? Yes or No: " % get_close_matches(w,data.keys())[0])
-		if yesno == "Y" or "y" or "Yes" or "yes":
-			return data [get_close_matches(w,data.keys())[0]]
-		elif yesno == "N" or "n" or "No" or "no":
+		yesno = input("Did you mean %s instead? Yes or No: " % get_close_matches(w,data.keys())[0])
+		if yesno == "Y": # or "y" or "Yes" or "yes"
+			return data[get_close_matches(w,data.keys())[0]]
+		elif yesno == "N": # or "n" or "No" or "no"
 			return "The word does not matched. Please double check again"
 		else:
 			return "We did not understand"
 	else :
-		return "The word not found"
+		return "Word not found"
 
-word = input("Enter your word: ")
-output = translate(word) #call the function
-if type(output) == list:
-	for item in output:
-		print(item)
-else:
-	print(output)
+#while True:
+
+word = ""
+while word != "!":
+	word = input("Enter your word(Enter ! to end the program): ")
+
+	output = translate(word) #call the function
+
+	if type(output) == list:
+		for item in output:
+			print(item)
+	else:
+			print(output)
